033023044367d3a519f81a7d1ef99085bb075b41
Chip Oglesby <chip@creativesparkcolumbia.com>
2017-07-06 09:00:54 -0600

    Updating analysis

--- r/code/analysis.R
+++ r/code/analysis.R
@@ -21,14 +21,6 @@ top10 <- salaries %>%
   top_n(10, salary)
 
 salaries %>% 
-  filter(tolower(position) == 'senior policy advisor') %>% 
-  arrange(year) %>% 
-  group_by(year) %>% 
-  top_n(1, salary) %>% 
-  ggplot(aes(year, salary)) +
-  geom_line()
-
-salaries %>% 
   filter(!is.na(status), year == 2015) %>% 
   ggplot(aes(status, salary)) +
   geom_boxplot()
@@ -37,14 +29,6 @@ longestStaff <- salaries %>%
   group_by(employee_name) %>% 
   summarize(years = n()) %>% 
   filter(years == 16)
-
-for (i in 1:length(longestStaff$employee_name)) {
-  print(salaries %>% 
-    filter(grepl(longestStaff$employee_name[[i]], employee_name)) %>% 
-    ggplot(aes(year, salary)) +
-    geom_line() +
-    ggtitle(longestStaff$employee_name[[i]]))
-}
 
 yearCount <- salaries %>% 
   group_by(employee_name) %>% 
@@ -64,82 +48,7 @@ positions <- salaries %>%
 specialAssistant <- salaries %>% 
   filter(position == 'special assistant to the president and associate counsel to the president')
 
-specialAssistant %>% 
-  group_by(year) %>% 
-  mutate(salary = median(salary)) %>% 
-  ggplot(aes(year, salary)) +
-  geom_line() +
-  ggtitle(specialAssistant$position[[1]])
-
-for(p in 1:10) {
-  print(salaries %>% 
-          # filter(grepl('assistant to the president', position)) %>% 
-          filter(grepl(positions$position[[p]], position)) %>% 
-          group_by(year) %>% 
-          # group_by(position) %>% 
-          # summarize(n = n())
-          mutate(salary = median(salary)) %>% 
-          ggplot(aes(year, salary)) +
-          geom_line() +
-          ggtitle(positions$position[[p]]))
-}
-
-## Office Analysis
-
-# Counselor To The President: https://en.wikipedia.org/wiki/Counselor_to_the_President
-ggplotly(salaries %>% 
-                   filter(grepl('bartlett, dan|gillespie, ed|rouse, pe|
-                                podesta, jo|conway, kel|bannon, ste', 
-                                employee_name)) %>% 
-                   group_by(year) %>% 
-                   summarize(medianSalary = median(salary)) %>% 
-                   ggplot(aes(year, medianSalary)) +
-                   geom_line(size = 1.5) +
-                   ggtitle('Counselor To The President - Median Salary'))
-
-# White House Chief of Staff: https://en.wikipedia.org/wiki/List_of_White_House_Chiefs_of_Staff
-ggplotly(salaries %>% 
-                   filter(grepl('card, an|bolten, jo|emanuel, ra|rouse, pe|
-                                daley, bi|lew, ja|mcdonough, de|priebus, re',
-                                employee_name)) %>% 
-                   group_by(year) %>% 
-                   summarize(medianSalary = median(salary)) %>% 
-                   ggplot(aes(year, medianSalary)) +
-                   geom_line(size = 1.5) +
-                   ggtitle('Chief of Staff - Median Salary'))
-
-# Executive Office of The President: 
-# https://en.wikipedia.org/wiki/Executive_Office_of_the_President_of_the_United_States
-ggplotly(salaries %>% 
-                   filter(grepl('assistant to the president', position)) %>%
-                   mutate(level = ifelse(grepl('^assistant', position), 'senior assistant', 
-                                         ifelse(grepl('^special', position), 
-                                                'special assistant', 'deputy assistant'))) %>% 
-                   group_by(year, level) %>%
-                   summarize(n = n(), 
-                       medianSalary = median(salary)) %>% 
-                   ggplot(aes(year, medianSalary)) +
-                   geom_line(aes(color = level)))
-
-# National Security Advisor: https://en.wikipedia.org/wiki/National_Security_Advisor_(United_States)
-ggplotly(salaries %>% 
-                   filter(grepl('donilon, to|rice, su|jones, ja|hadley, st|
-                                rice, con|flynn, mi|kellogg, ke|mcmaster, h',
-                                employee_name)) %>% 
-                   group_by(year) %>% 
-                   summarize(medianSalary = median(salary)) %>% 
-                   ggplot(aes(year, medianSalary)) +
-                   geom_line() +
-                   ggtitle('National Security Advisor - Median Salary'))
-
-ggplotly(salaries %>%
-                  filter(grepl('lady', position)) %>% 
-                  group_by(year) %>% 
-                  summarize(medianSalary = median(salary)) %>% 
-                  ggplot(aes(year, medianSalary)) +
-                  geom_line() +
-                  ggtitle('Office Of The First Lady - Median Salary'))
-
+## Office Analysis:
 # White House Counsel: https://en.wikipedia.org/wiki/White_House_Counsel
 counselToPresident <- salaries %>% 
   filter(grepl('miers, ha|fielding, fr|craig, gr|bauer|ruemmler|
@@ -150,30 +59,13 @@ counselToPresident <- salaries %>%
 
 ## Gender Analysis
 salaries %>% 
-  group_by(gender) %>%
-  tally() %>% 
-  ggplot(aes(gender, n)) +
-  geom_bar(stat = 'identity')
-
-salaries %>% 
-  filter(year == 2017, status == 'employee') %$% 
+  filter(status == 'employee') %$% 
   t.test(salary[gender == 'male'], salary[gender == 'female'])
 
-salaries %>% 
-  ggplot(aes(gender, salary, fill = gender)) +
-  geom_point(position=position_jitter(width=.11),col="grey") +
-  geom_boxplot(alpha=0.5)
-
-salaries %>%
-  group_by(year = as.factor(year)) %>% 
-  ggplot(aes(year, salary, fill = gender)) +
-  geom_boxplot(alpha = .5) +
-  ggtitle('Boxplot: Gender & Salaries')
-
-ggplotly(salaries %>%
-           filter(status == 'employee') %>% 
-           group_by(year, gender) %>%
-           summarize(medianSalary = median(salary)) %>% 
-           ggplot(aes(year, medianSalary)) +
-           geom_line(aes(color = gender)) +
-           ggtitle('White House Salaries: Men vs Women'))
+wageGap <- salaries %>% 
+  group_by(year) %>% 
+  summarize(wageGap = 
+              round(((median(salary[gender == 'female'])/40) /
+                       (median(salary[gender == 'male'])/40) * 100), 2),
+            mensMedianSalary = median(salary[gender == 'male']),
+            womensMedianSalary = median(salary[gender == 'female']))
